c424817554d1d49285ddc6d138f079d8e95ea8ab
Jorge Moreno <jmoren@gmail.com>
2011-09-16 16:59:56 -0300

    change tabs orientation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(function(){
-  $('.tipsy-s').tipsy({gravity: $.fn.tipsy.autoNS});
-  $('.tipsy-e').tipsy({gravity: $.fn.tipsy.autoWE,html: true});
+  $(function(){
+    $("*[data-placeholder]").bind('focus', unhold );
+    $("*[data-placeholder]").bind('blur', hold );
+    $("*[data-placeholder]").each(hold);
+  });
+  $('.tipsy-s').tipsy({gravity: 's'});
+  $('.tipsy-e').tipsy({gravity: 'e',html: true});
+
   $('.delete_task').click(function(){
-    var p = $(this).attr('href')
+    var p = this.href
     id = p.split('/')[2]
     $('#'+id).find('.task').html('<div><img src="/images/loading.gif"> eliminado...</div>')
   });
@@ -31,4 +37,12 @@ $(function(){
   });
 
 });
+var unhold = function(e){
+  $(this).val('');
+}
+var hold = function(e){
+  if ($(this).val() == ''){
+    $(this).val($(this).attr('data-placeholder'));
+  }
+}
 
